bddfe1208799a5e76be50a1aee5cc684d6c3a9af
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2022-05-29 20:35:45 -0300

    Ex obrigatório 1: Calculadora

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/ex 2/script.js
@@ -0,0 +1,13 @@
+function calc(e){
+    var n1=parseFloat(document.getElementById("1").value);
+    var n2=50
+    var resto = (n1%n2);
+    
+    if (resto == 0); {
+       alert(resto);
+    }
+    if (resto != 0); {
+       alert(document.getElementById("1").value);
+    }
+ 
+ }
