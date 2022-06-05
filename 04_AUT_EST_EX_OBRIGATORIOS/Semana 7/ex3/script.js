812d940806cdf1932090b2055f190ce2b22d9dd6
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2022-06-05 19:13:13 -0300

    Etapa 7
    Exercício obrigatório 3

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex3/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex3/script.js
@@ -1,9 +1,32 @@
 function confirmar() {
-    var arr = [document.getElementById('t').value.length = 0, 
-    document.getElementById('t').value.length = 1];
-    // arr.push(document.getElementById('t').value)
-  
-    document.querySelector(".a").innerHTML = arr[length]=0;
+    t = document.getElementById('t').value;
 
+    // aqui guarda em um array todos os dígitos inseridos no input:
+    arr = t.split("");
+    
+    if (
+    
+    //aqui verifica se os parênteses e o traço foram inseridos corretamente:
+    arr[0] == '(' && arr[3] == ')' && arr[9] == '-' &&
+
+    //aqui verifica se foram usados apenas números:
+    !isNaN(arr[1]) &
+    !isNaN(arr[2]) &
+    !isNaN(arr[4]) &
+    !isNaN(arr[5]) &
+    !isNaN(arr[6]) &
+    !isNaN(arr[7]) &
+    !isNaN(arr[8]) &
+    !isNaN(arr[10]) &
+    !isNaN(arr[11]) &
+    !isNaN(arr[12]) &
+    !isNaN(arr[13]) 
+    ) 
+    
+    {
+        document.querySelector(".a").innerHTML = "Tudo certo!"
+    }
+    else {
+        document.querySelector(".a").innerHTML = "Não foi isso que eu pedi :("
+    }
 }
-// EU TENTEI DE TUDO MAS NÃO SEI COMO CHAMAR CADA ALGARISMO DO INPUT, DESISTO
